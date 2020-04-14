3144bb2185bf5366af82f839b5fab8f10fd347e3
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2020-04-14 14:27:29 +0300

    added getters and constructor

--- Animal.java
+++ Animal.java
@@ -3,5 +3,22 @@ public class Animal
     private int weight, maxAge;
     private String id, name, type, animalName;
 
-    
+    public Animal(int w, int m, String i, String n, String t, String a)
+    {
+        weight = w;
+        maxAge = m;
+        id = i;
+        name = n;
+        type = t;
+        animalName = a;
+    }
+
+    //getters για τις τιμές των ζώων
+    public int    getWeight()      {return weight;}
+    public int    getMaxAge()      {return maxAge;}
+    public String getId()          {return id;}
+    public String getName()        {return name;}
+    public String getType()        {return type;}
+    public String getAnimalName()  {return animalName;}
+
 }
