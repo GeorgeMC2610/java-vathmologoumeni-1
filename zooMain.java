e67fb9c35cfdef2e1ec5e28bb97d9b68bed6fe3b
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2020-04-14 17:26:55 +0300

    added the main menu

--- zooMain.java
+++ zooMain.java
@@ -6,6 +6,19 @@ public class zooMain
     {
         Scanner input = new Scanner(System.in);
 
+        //εμφάνιση του μενού με διαδοχικά prints.
         System.out.println("ΓΙΩΡΓΟΣ ΣΕΪΜΕΝΗΣ, ΠΑΝΕΠΙΣΤΗΜΙΟ ΠΕΙΡΑΙΩΣ ΤΜΗΜΑ ΠΛΗΡΟΦΟΡΙΚΗΣ, Π19204");
+
+
+        System.out.println("\n\nΚαλώς ορίσατε στο μενού του Ζωολογικού Κήπου. Ενέργειες:\n");
+        System.out.println("'1' Προβολή όλων των διαθέσιμων ζώων");
+        System.out.println("'2' Προσθήκη νέου ζώου");
+        System.out.println("'3' Αναζήτηση με όνομα");
+        System.out.println("'4' Αναζήτηση με κωδικό");
+        System.out.println("'5' Επεξεργασία ζώου");
+        System.out.println("'6' Διαγραφή ζώου");
+        System.out.println("'7' ΕΞΟΔΟΣ\n");
+        
+        System.out.println("\nΕισάγετε ενέργεια: ");
     }
 }
