1d91e8b9cdedf658b00c4eba5a13180c1d10ab7d
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2020-04-14 14:16:15 +0300

    making of the programs

---
+++ Animal.java
@@ -0,0 +1,7 @@
+public class Animal
+{
+    private int weight, maxAge;
+    private String id, name, type, animalName;
+
+    
+}
---
+++ zooMain.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class zooMain
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
